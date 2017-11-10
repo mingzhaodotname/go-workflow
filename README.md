9e596d48964d5feb2490ce349bcff8f4d0c6655a
Ming Zhao <mingzhaodotname@users.noreply.github.com>
2017-11-10 15:15:18 -0800

    Enable orm for database access

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # go-workflow
 Workflow in Golang
+
+# TODOs
+* generate uuid when creating workflow template
+* add more fields to template, e.g.
+  * create time, user
+  * update time, user
--- wf-server/main.go
+++ wf-server/main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	_ "github.com/mingzhaodotname/go-workflow/wf-server/routers"
-
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
 )
+
+func init() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+
+	orm.RegisterDataBase("default", "mysql", "root:000000@/wfserver?charset=utf8")
+}
 
 func main() {
 	if beego.BConfig.RunMode == "dev" {
